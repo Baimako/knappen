f3d5a8af3245d087eb3b84ac1deca1a394a9a347
Baimako <sascha.bergenas@hotmail.com>
2025-09-28 16:19:38 +0200

    la in function att bakgrunden blir blå on click och går tillbaka till default efter 3 sec

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const btn = document.querySelector("#my-button"); // hämtar element med my-button (id="my-button")
+const btn = document.querySelector("#my-button"); // skapar en variabel med värdet btn(kan välja själv),
+//  hittar elementet med id my-button i index
+// (id="my-button")
 // sparar elementet i en variabel som heter btn.
 btn.addEventListener("click", () => {
   //lägg till vad som händer när man klickar på knappen
   alert("Ikuuuuuu~!");
+  document.body.classList.toggle("blue-bg"); // ändrar bakgrundsfärgen on click
+  setTimeout(() => {
+    // tar bort bakrundsfärgen efter 3 sec
+    document.body.classList.remove("blue-bg");
+  }, 3000);
 });
